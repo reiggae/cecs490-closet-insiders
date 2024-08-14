b82dc052e0ad68231fcdf0551533ce2fba8f44a6
NicaivanCharles <bayaras.nicaivancharles@gmail.com>
2024-08-14 16:45:26 -0700

    RFID (Working) Attempt to Board To Board

--- arduino/Arduino_closetinsiders.ino
+++ arduino/Arduino_closetinsiders.ino
@@ -3,51 +3,34 @@
 
 #define SS_PIN 10
 #define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN); // creating the general instance of the Card Reader
+MFRC522 mfrc522(SS_PIN, RST_PIN); // setting up the Start of the Reader
 
-void setup(){ // why the fuck is the naming convention so stupid
-// General setup to run a single time
-  Serial.begin(9600);
-  SPI.begin();
-  mfrc522.PCD_Init();
-  Serial.println("Please hold the ID card you want to read..");
-  Serial.println();
+void setup() {
+  Serial.begin(9600); // Init serial communication
+  SPI.begin(); // Init the bus
+  mfrc522.PCD_Init(); // Init RFID 
+  Serial.println("Please Scan the Clothing item....");
 }
 
-
-void loop(){
-  //checks if the ID card is being help present
-  if(!mfrc522.PICC_IsNewCardPresent()) {
-    return; // if not card is being detected
-  }
-  //read the information
-  if(!mfrc522.PICC_ReadCardSerial()){
-    return;
+void loop() {
+  // New Card? OOh it works
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return; // No Card? Womp Womp
   }
 
-  Serial.print("Card ID: ");
-  String cardID = ""; //Stores the ID of the card 
-  for (byte i = 0; i<mfrc522.uid.size; i++){
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ?" 0" : " " );         // formats the ID
-    Serial.print(mfrc522.uid.uidByte[i], HEX);                        // prints in Hex
-    cardID.concat(String(mfrc522.uid.uidByte[i]< 0x10 ?" 0" : " " )); //stores the ID
-    cardID.concat(String(mfrc522.uid.uidByte[i], HEX));
-}
-Serial.println();
-
-// Check if the card ID matches a specific authorized ID
-
-cardID.toUpperCase();
-// D3 BB 86 11 - Card
-// A3 3A 33 1B - Gym Tag
-if(cardID.substring(1) == "A3 3A 33 1B")
-{
-  Serial.println("Good Job, You actually did it: ");
-  Serial.println();
-  delay(3000); // a small delay
-  } else {
-  Serial.println("Womp, Womp");
-  Serial.println();
-  delay(3000);
+  // Try to read the card's serial number
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return; // Could not read the card, exit the loop
   }
+
+  // Create a string to store the card ID
+  String cardID = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    cardID += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+    cardID += String(mfrc522.uid.uidByte[i], HEX);
+  }
+
+  // Send the card ID to the Raspberry Pi
+  Serial.println(cardID);
+  delay(1000); // Add a delay to prevent flooding the serial buffer
 }
