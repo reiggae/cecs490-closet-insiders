380c7483f6517bc9859fc91de29756964e78be8f
NCBayaras <147568273+NCBayaras@users.noreply.github.com>
2024-09-30 12:10:15 -0700

    Arduino_closetinsiders.ino
    Just sends the ID Code

--- arduino/Arduino_closetinsiders.ino
+++ arduino/Arduino_closetinsiders.ino
@@ -3,207 +3,34 @@
 
 #define SS_PIN 10 //SDA
 #define RST_PIN 9 //RST
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Creating an instance of the Card Reader
-
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 // SCK 13, MOSI 11, MISO 12
-
-#define ClothingCapacity 10 // A Cap on the size of the closet (could be increased)
-#define UserCapacity 2  // A Cap on the number of authorized users
-
-// Arrays for registered items and users
-String clothingItem[ClothingCapacity] = {"A3 3A 33 1B", "B3 10 4C F5", "C3 26 00 9C"};
-bool isCheckedIn[ClothingCapacity] = {true, true, true}; // Status if it's inside the closet
-int registeredItemCount = 3;
-
-String registeredUsers[UserCapacity] = {"D3 BB 86 11"};
-int registeredUserCount = 1; // currently only one person is registered in the system
-
 void setup() {
-  Serial.begin(9600);
-  SPI.begin();
-  mfrc522.PCD_Init();
-  Serial.println("Please Bring Up The Item You Want to Scan");
-  Serial.println();
+  Serial.begin(115200);
+  SPI.begin();        // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522
 }
 
+
 void loop() {
-  // Checks if a new card is present
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-    return; // If no card is detected
-  }
-  if (!mfrc522.PICC_ReadCardSerial()) {
+  // Look for new cards
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+    delay(50);
     return;
   }
 
-  Serial.print("Card ID: ");
-  String cardID = ""; // Stores the ID of the card 
+  // Card detected, send the ID
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    if (mfrc522.uid.uidByte[i] < 0x10) {
+      Serial.write('0'); // Send a leading zero for values less than 0x10
+    }
     Serial.print(mfrc522.uid.uidByte[i], HEX);
-    cardID.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    cardID.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  Serial.println();
+  Serial.println(); // Send a newline to indicate end of ID
 
-  cardID.toUpperCase(); // Convert to uppercase
+  // Halt PICC and get ready for new read
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
 
-  bool isRegisteredUser = false;
-  for (int i = 0; i < registeredUserCount; i++) {
-    if (cardID.substring(1) == registeredUsers[i]) {
-      isRegisteredUser = true;
-      break;
-    }
-  }
-
-  if (isRegisteredUser) {
-    // Allow the user to handle item registration or checking in/out
-    handleAuthorizedUser();
-  } else {
-    Serial.println("Access Denied: Unauthorized User");
-    Serial.println();
-    delay(3000);
-  }
+  delay(1000);  // Wait a bit before next read
 }
-
-void handleAuthorizedUser() {
-  Serial.println("Authorized User Detected");
-  Serial.println("Please Scan an Item to Check In/Out or Register");
-  
-  while (true) {
-    // Wait for the user to scan a card
-    if (!mfrc522.PICC_IsNewCardPresent()) {
-      continue; // If no card is detected, continue waiting
-    }
-    if (!mfrc522.PICC_ReadCardSerial()) {
-      continue;
-    }
-
-    Serial.print("Card ID: ");
-    String cardID = ""; // Stores the ID of the card 
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      Serial.print(mfrc522.uid.uidByte[i], HEX);
-      cardID.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-      cardID.concat(String(mfrc522.uid.uidByte[i], HEX));
-    }
-    Serial.println();
-
-    cardID.toUpperCase(); // Convert to uppercase
-
-    // Check if the scanned card is a registered user card
-    bool isRegisteredUser = false;
-    for (int i = 0; i < registeredUserCount; i++) {
-      if (cardID.substring(1) == registeredUsers[i]) {
-        isRegisteredUser = true;
-        delay(3000);
-        break;
-      }
-    }
-
-    if (isRegisteredUser) {
-      // If the user scans their card again, break the loop and exit
-      Serial.println("Exiting Authorized Mode");
-      Serial.println();
-      delay(3000);
-      return;
-    }
-
-    handleItemRegistration(cardID);
-    
-    // Add a delay to prevent the same card from being scanned twice quickly
-    delay(3000); // 3 second delay, can be adjusted if necessary
-  }
-}
-
-void handleItemRegistration(String cardID) {
-  bool isRegisteredItem = false;
-  int itemIndex = -1;
-  for (int j = 0; j < registeredItemCount; j++) {
-    if (cardID.substring(1) == clothingItem[j]) {
-      isRegisteredItem = true;
-      itemIndex = j;
-      break;
-    }
-  }
-
-  if (isRegisteredItem) {
-    // Toggle the status of the item
-    isCheckedIn[itemIndex] = !isCheckedIn[itemIndex];
-    if (isCheckedIn[itemIndex]) {
-      Serial.println("Clothing Item Checked In");
-    } else {
-      Serial.println("Clothing Item Checked Out");
-    }
-    Serial.println();
-    delay(1000); // A small delay
-  } else {
-    Serial.println("Clothing Item Has Not Been Registered");
-    char response;
-
-    // Prompt the user to register the item until they provide a correct response (Y/N)
-    do {
-      Serial.println("Do you want to register this clothing item? (Y/N)");
-      while (!Serial.available()); // Wait until something is entered
-      response = Serial.read(); // Read the input
-      response = toupper(response); // Convert to uppercase
-    } while (response != 'Y' && response != 'N');
-
-    if (response == 'Y') {
-      // Register the new card if there is space in the array
-      if (registeredItemCount < ClothingCapacity) {
-        clothingItem[registeredItemCount] = cardID.substring(1);
-        isCheckedIn[registeredItemCount] = true; // Initially checked in
-        registeredItemCount++;
-        Serial.println("Item Registered Successfully");
-      } else {
-        Serial.println("Registration Failed: User Has Declined to Register Clothing Item");
-      }
-    } else {
-      Serial.println("Clothing Item Not Registered");
-    }
-    Serial.println();
-    delay(1000);
-  }
-}
-
-// setup for the serial communication between Arduino and Pi
-// Arduino Uno -> Pi 5 Connections
-//     Connect the GND pin to the Raspberry Pi’s GND.
-//     Connect the TX (Pin 1) of the Arduino Uno to the RX pin (GPIO 15, Pin 10) of the Raspberry Pi.
-//     Connect the RX (Pin 0) of the Arduino Uno to the TX pin (GPIO 14, Pin 8) of the Raspberry Pi.
-
-// #include <SPI.h>
-// #include <MFRC522.h>
-
-// #define SS_PIN 10 // SDA
-// #define RST_PIN 9 // RST
-// MFRC522 mfrc522(SS_PIN, RST_PIN); // Creating an instance of the Card Reader
-
-// void setup() {
-//   Serial.begin(9600);     // Initialize serial communication
-//   SPI.begin();            // Initialize SPI bus
-//   mfrc522.PCD_Init();     // Initialize MFRC522 RFID reader
-//   Serial.println("Waiting for card...");
-// }
-
-// void loop() {
-//   // Checks if a new card is present
-//   if (!mfrc522.PICC_IsNewCardPresent()) {
-//     return; // If no card is detected
-//   }
-//   if (!mfrc522.PICC_ReadCardSerial()) {
-//     return;
-//   }
-
-//   // Retrieve and send the card ID to Raspberry Pi
-//   String cardID = ""; // Stores the ID of the card
-//   for (byte i = 0; i < mfrc522.uid.size; i++) {
-//     cardID.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-//     cardID.concat(String(mfrc522.uid.uidByte[i], HEX));
-//   }
-
-//   cardID.toUpperCase(); // Convert to uppercase
-//   Serial.println(cardID); // Send the card ID over Serial to Raspberry Pi
-//   delay(1000); // Delay to prevent immediate repeat scans
-// }
-
