9b776084e1ddd21d07dd48f1c0f908496b802670
Andrew68-lab <80541585+Andrew68-lab@users.noreply.github.com>
2024-11-04 13:40:37 -0800

    Update print_closet.py
    Added Rack

--- raspberry_pi/print_closet.py
+++ raspberry_pi/print_closet.py
@@ -1,6 +1,9 @@
 # Function to print all clothings in the closet
 def print_closet(closet):
+    rack = []
     print("Closet contents:")
     for i, clothing in enumerate(closet):
+        rack.append(i)
         print(f"Clothing {i + 1}:")
         clothing.print()
+    print(f"Rack List: {rack}")
